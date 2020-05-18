7ecce259b033a04080bf52a6eb9dd28fef1f0763
sankeths <s.sanketh@gmail.com>
2020-05-18 11:13:03 +0530

    index.js
    Added instructions to keep mouse within gamepad

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ document.addEventListener('mouseenter', onMouseUpdate, false);
 mousemove_block.onclick = function(){
     game_restart = "True";
     if(game_inprogress != "True"){
-        StartText.innerText = "Move Mouse within this game pad ";
+        StartText.innerText = "Move Mouse inside this game pad ";
         Runner();
     }
 };
